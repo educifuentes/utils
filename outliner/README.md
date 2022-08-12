2eb2a78dceded044c75290043d5ad140c8317974
Eduardo Cifuentes <edu.cifuentes@gmail.com>
2022-08-12 19:12:45 -0400

    feat: add outliner

---
+++ outliner/README.md
@@ -0,0 +1,17 @@
+# Outliner
+
+Copy a text and transform to bulleted list. Useful to add content to Logseq.
+
+## Usage
+
+1. Copy text you want to transform (add to clipboard)
+2. Run
+```python
+  python3 outliner.py
+```
+
+## References
+- [Python - Convert simple lines to bulleted lines using the Pyperclip module - GeeksforGeeks](https://www.geeksforgeeks.org/python-convert-simple-lines-to-bulleted-lines-using-the-pyperclip-module/)
+
+### Pendings
+- Match chapter line with `\d.` and replace
---
+++ outliner/paperclip.py
@@ -0,0 +1,27 @@
+import pyperclip
+
+
+# saves text copied to clipboard
+# in variable text
+text = pyperclip.paste()
+print("Before Modification:")
+print(text)
+
+# stores different lines of text
+# in a list named lines
+lines = text.split("\n")
+
+# adds * to every line stored
+# in list
+for i in range(len(lines)):
+    lines[i] = "- " + lines[i].strip()
+
+# converts the list of different
+# lines to single text
+text = "\n".join(lines)
+
+# copies new modified text
+# to the clipboard
+pyperclip.copy(text)
+print("\nAfter Modification:")
+print(pyperclip.paste())
